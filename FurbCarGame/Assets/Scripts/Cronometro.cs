32e9f6f4f1327841d902d8d1ebf71357c697a709
gustavobruder <gugabruder@gmail.com>
2026-05-10 23:02:42 -0300

    ajustado script Cronometro - ajustado para salvar o tempo da melhor volta

--- FurbCarGame/Assets/Scripts/Cronometro.cs
+++ FurbCarGame/Assets/Scripts/Cronometro.cs
@@ -4,8 +4,10 @@ using UnityEngine.UI;
 public class Cronometro : MonoBehaviour
 {
     public Text textoCronometro;
+    public Text textoCronometroMelhorTempo;
 
     private float _tempo = 0f;
+    private float _melhorTempo = 0f;
     private bool _cronometrando = true;
 
     private void Update()
@@ -13,19 +15,45 @@ public class Cronometro : MonoBehaviour
         if (_cronometrando)
         {
             _tempo += Time.deltaTime;
-            AtualizarCronometro();
+            AtualizarCronometroTempo();
         }
     }
 
-    private void AtualizarCronometro()
+    private void AtualizarCronometroTempo()
     {
+        if (textoCronometro == null) return;
+
         var minutos = Mathf.FloorToInt(_tempo / 60);
         var segundos = Mathf.FloorToInt(_tempo % 60);
-        textoCronometro.text = $"{minutos:00}:{segundos:00}";
+        textoCronometro.text = $"Tempo: {minutos:00}:{segundos:00}";
+    }
+
+    private void AtualizarCronometroMelhorTempo()
+    {
+        if (textoCronometroMelhorTempo == null) return;
+
+        var minutos = Mathf.FloorToInt(_melhorTempo / 60);
+        var segundos = Mathf.FloorToInt(_melhorTempo % 60);
+        textoCronometroMelhorTempo.text = $"Melhor: {minutos:00}:{segundos:00}";
+    }
+
+    public void IniciarCronometro()
+    {
+        _tempo = 0f;
+        _cronometrando = true;
     }
 
     public void PararCronometro()
     {
-        if (_cronometrando) _cronometrando = false;
+        if (_cronometrando)
+        {
+            _cronometrando = false;
+
+            if (_melhorTempo == 0 || _tempo < _melhorTempo)
+            {
+                _melhorTempo = _tempo;
+                AtualizarCronometroMelhorTempo();
+            }
+        }
     }
 }
